b0aa0cd30f15e88d0c0e1d8ca4faa92131d36812
Ronnie Nyaga <ronnienyaga@gmail.com>
2020-02-27 12:14:30 +0300

    Determine user

--- App.js
+++ App.js
@@ -1,12 +1,48 @@
 import React, { Component } from 'react';
-import { Platform, StyleSheet, Text, View } from 'react-native';
+import { Platform, StyleSheet, Text, View, AsyncStorage } from 'react-native';
+import SocketIOClient from 'socket.io-client';
+import { GiftedChat } from 'react-native-gifted-chat';
 
 const instructions = Platform.select({
   ios: 'Press Cmd+R to reload,\n' + 'Cmd+D or shake for dev menu',
   android: 'Double tap R on your keyboard to reload,\n' + 'Shake or press menu button for dev menu',
 });
 
+const USER_ID = '@userId';
+
 export default class App extends Component {
+  state = {
+    messages: [],
+    userId: null,
+  };
+
+  constructor (props) {
+    super(props);
+
+    this.socket = SocketIOClient('http://localhost:3000');
+    this.socket.on('message', this.onReceiveMessage);
+    this.determineUser();
+  }
+
+  determineUser = () => {
+    AsyncStorage.getItem(USER_ID)
+      .then((userId) => {
+        if (!userId) {
+          this.socket.emit('userJoined', null);
+          this.socket.on('userJoined', (userId) => {
+            AsyncStorage.setItem(USER_ID, userId);
+            this.setState({ userId });
+          });
+        } else {
+          this.socket.emit('userJoined', userId);
+          this.setState({ userId });
+        }
+      }).catch((err) => console.log(err));
+  };
+  onReceiveMessage = () => {};
+  onSend = () => {};
+  _storeMessages = () => {};
+
   render() {
     return (
       <View style={styles.container}>
